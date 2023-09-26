f7899e1a600d1c5215c3b50021a2a8a95dbc8221
jelle-michiels <jelle.michiels1@student.ucll.be>
2023-09-26 16:18:13 +0200

    Added control toggle

---
+++ Assets/Scripts/Creator-mode/ControlToggle.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ControlToggle : MonoBehaviour
+{
+    public RectTransform controlToggle;
+    public Color backgroundActiveColor;
+    public Toggle toggle;
+    public Image backgroundImage;
+    public GameObject controlMenu;
+
+    Color backgroundDefaultColor;
+
+    Vector2 handlePosition;
+
+    void Start()
+    {
+        handlePosition = controlToggle.anchoredPosition;
+
+        backgroundImage = controlToggle.parent.GetComponent<Image>();
+        backgroundDefaultColor = backgroundImage.color;
+
+        toggle.onValueChanged.AddListener(OnSwitch);
+
+        if (toggle.isOn)
+        {
+            OnSwitch(true);
+
+        }
+
+    }
+
+    void OnSwitch(bool on)
+    {
+        controlToggle.DOAnchorPos(on ? handlePosition * -1 : handlePosition, .4f).SetEase(Ease.InOutBack);
+        backgroundImage.DOColor(on ? backgroundActiveColor : backgroundDefaultColor, .6f);
+    }
+
+    void OnDestroy()
+    {
+        toggle.onValueChanged.RemoveListener(OnSwitch);
+    }
+
+    public void ToggleControlMenu()
+    {
+        controlMenu.SetActive(!controlMenu.activeSelf);
+    }
+}
