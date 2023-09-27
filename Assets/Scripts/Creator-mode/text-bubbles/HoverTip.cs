7d46c185c139f52f635b48a152ef06e823f57fa0
Jasper Coddens <jcoddens259@gmail.com>
2023-09-27 13:29:15 +0200

    Hover tip works, cleanup required

--- Assets/Scripts/Creator-mode/text-bubbles/HoverTip.cs
+++ Assets/Scripts/Creator-mode/text-bubbles/HoverTip.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class HoverTip : MonoBehaviour
+public class HoverTip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    public string tipToShow;
+    private float timeToWait = 0.5f;
+
+    public void OnPointerEnter(PointerEventData eventData)
     {
-        
+        StopAllCoroutines();
+        StartCoroutine(StartTimer());
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerExit(PointerEventData eventData)
     {
-        
+        StopAllCoroutines();
+        HoverTipManager.OnMouseLoseFocus();
+    }
+
+    private void ShowMessage()
+    {
+        HoverTipManager.OnMouseHover(tipToShow, Input.mousePosition);
+    }
+
+    private IEnumerator StartTimer()
+    {
+        yield return new WaitForSeconds(timeToWait);
+
+        ShowMessage();
     }
 }
--- Assets/Scripts/Creator-mode/text-bubbles/HoverTipManager.cs
+++ Assets/Scripts/Creator-mode/text-bubbles/HoverTipManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class HoverTipManager : MonoBehaviour
 {
