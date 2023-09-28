e12fe533bbee75e0b6263824711549e2fa00f495
jelle-michiels <jelle.michiels1@student.ucll.be>
2023-09-28 16:05:11 +0200

    Controller navigation 'working'
    It works for home > game > option menu > game > home.

--- Assets/Scripts/player/EscapeMenu.cs
+++ Assets/Scripts/player/EscapeMenu.cs
@@ -2,6 +2,7 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class EscapeMenu : MonoBehaviour
@@ -12,6 +13,8 @@ public class EscapeMenu : MonoBehaviour
     public static bool isPaused = false;
 
     public GameObject escapeMenu, playerTipsMenu, optionsMenu;
+    
+    public GameObject escapeMenuFirst, optionsMenuFirst, optionMenuButton;
 
     public PlayerControl playerControl;
 
@@ -51,6 +54,8 @@ public class EscapeMenu : MonoBehaviour
     void OnEscape(bool shown)
     {
         escapeMenu.SetActive(shown ? false : true);
+        EventSystem.current.SetSelectedGameObject(shown ? null : escapeMenuFirst);
+
     }
 
     public void Resume()
@@ -72,5 +77,15 @@ public class EscapeMenu : MonoBehaviour
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("Menu");
+    }
+
+    public void OpenOptionMenu()
+    {
+        EventSystem.current.SetSelectedGameObject(optionsMenuFirst);
+    }
+
+    public void CloseOptionMenu()
+    {
+        EventSystem.current.SetSelectedGameObject(optionMenuButton);
     }
 }
