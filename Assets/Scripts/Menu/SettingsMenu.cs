7eee5f2b29e3a021430e9a96686adb92df74c2f0
Jasper Coddens <jcoddens259@gmail.com>
2023-10-06 12:48:16 +0200

    min timer removed

--- Assets/Scripts/Menu/SettingsMenu.cs
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -7,56 +7,32 @@ using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
-/*    public InputField maxTime;
-    public InputField minTime;*/
 
-    public Slider minSlider, maxSlider;
-    public TextMeshProUGUI minSliderValue, maxSliderValue;
+    public Slider maxSlider;
+    public TextMeshProUGUI maxSliderValue;
 
     public static string maxTimeText = "3600";
     public static string minTimeText = "0";
 
-/*    public Toggle setActive;*/
-
     void Start()
     {
-        /*setActive = GameObject.Find("MakeActiveToggle").GetComponent<Toggle>();
-        setActive.onValueChanged.AddListener(delegate { SetActiveLevel(); });*/
+        
     }
 
     public void addTimer()
     {
-/*        maxTimeText = maxTime.text;
-        minTimeText = minTime.text;*/
-
         Debug.Log("Max time: " + maxTimeText);
-        Debug.Log("Min time: " + minTimeText);
         string level = GetComponent<DropdownHandler>().value;
         Debug.Log("Selected: " + level);
 
         GetComponent<EditorDatabase>().addTimers(int.Parse(maxTimeText), int.Parse(minTimeText), level);
-        /*SceneManager.LoadScene("Menu");*/
     }
 
     public void SetActiveLevel()
     {
-        /*if (setActive.isOn)
-        {*/
-            string level = GetComponent<DropdownHandler>().value;
-            Debug.Log("Selected: " + level);
-            PlayerPrefs.SetString("ActiveLevel", level);
-        /*}
-        else if (!setActive.isOn)
-        {
-            PlayerPrefs.SetString("ActiveLevel", "");
-        }*/
-
-    }
-
-    public void UpdateMinSlider()
-    {
-        minSliderValue.text = minSlider.value.ToString();
-        minTimeText = minSlider.value.ToString();
+        string level = GetComponent<DropdownHandler>().value;
+        Debug.Log("Selected: " + level);
+        PlayerPrefs.SetString("ActiveLevel", level);
     }
 
     public void UpdateMaxSlider()
