af5f58d5a93045529be078f56d1d576b8c304105
jelle-michiels <jelle.michiels1@student.ucll.be>
2023-10-06 11:07:18 +0200

    Changed dropdown to arrow buttons

--- Assets/Scripts/Menu/SettingsMenu.cs
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -7,21 +7,49 @@ using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
-/*    public InputField maxTime;
-    public InputField minTime;*/
-
     public Slider minSlider, maxSlider;
-    public TextMeshProUGUI minSliderValue, maxSliderValue;
+    public Button downArrowButton, upArrowButton;
+    public TextMeshProUGUI minSliderValue, maxSliderValue, levelTextField;
+    public Sprite disabledDownArrowImage, disabledUpArrowImage, enabledDownArrowImage, enabledUpArrowImage;
+    public Image downArrowImage, upArrowImage;
 
     public static string maxTimeText = "3600";
     public static string minTimeText = "0";
 
-/*    public Toggle setActive;*/
+    private List<string> levels;
+    private int levelIndex;
 
     void Start()
     {
-        /*setActive = GameObject.Find("MakeActiveToggle").GetComponent<Toggle>();
-        setActive.onValueChanged.AddListener(delegate { SetActiveLevel(); });*/
+        levels = GetComponent<EditorDatabase>().GetLevels();
+
+        if (levels.Count == 0)
+        {
+            levelTextField.text = "No levels found";
+
+            downArrowButton.interactable = false;
+            upArrowButton.interactable = false;
+
+            downArrowImage.sprite = disabledDownArrowImage;
+            upArrowImage.sprite = disabledUpArrowImage;
+        }
+        else
+        {
+            LevelValueChanged(levels[0]);
+            levelIndex = 0;
+            downArrowImage.sprite = disabledDownArrowImage;
+            downArrowButton.interactable = false;
+        }
+    }
+
+
+    public void LevelValueChanged(string levelText)
+    {
+        Debug.Log("Selected: " + levelText);
+
+        levelTextField.text = levelText;
+
+        PlayerPrefs.SetString("ActiveLevel", levelText);
     }
 
     public void addTimer()
@@ -63,6 +91,40 @@ public class SettingsMenu : MonoBehaviour
     {
         maxSliderValue.text = maxSlider.value.ToString();
         maxTimeText = maxSlider.value.ToString();
+    }
+
+    public void getNextLevel()
+    {
+        if (levelIndex < levels.Count - 1)
+        {
+            levelIndex++;
+            LevelValueChanged(levels[levelIndex]);
+            downArrowImage.sprite = enabledDownArrowImage;
+            downArrowButton.interactable = true;
+        }
+
+        if (levelIndex == levels.Count - 1)
+        {
+            upArrowButton.interactable = false;
+            upArrowImage.sprite = disabledUpArrowImage;
+        }
+    }
+
+    public void getPreviousLevel()
+    {
+        if (levelIndex > 0)
+        {
+            levelIndex--;
+            LevelValueChanged(levels[levelIndex]);
+            upArrowImage.sprite = enabledUpArrowImage;
+            upArrowButton.interactable = true;
+        }
+
+        if (levelIndex == 0)
+        {
+            downArrowButton.interactable = false;
+            downArrowImage.sprite = disabledDownArrowImage;
+        }
     }
 
 }
