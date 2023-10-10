130e58963cf28fd96a50faaa09968eae70643300
Patryk Piekarz <patryk.piekarz@student.ucll.be>
2023-10-10 16:25:17 +0200

    hitboxes work

--- Assets/Scripts/player/InteractableObjectWarderobe.cs
+++ Assets/Scripts/player/InteractableObjectWarderobe.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class InteractableObjectWarderobe : MonoBehaviour
 {
@@ -9,16 +10,27 @@ public class InteractableObjectWarderobe : MonoBehaviour
 
     public GameObject attachedObject;
     
-
+    private bool playerInZone;
     enum ObjectState
     {
         filled,
         empty
     }
 
+    // any trigger laat het afgaan
+    private void OnTriggerEnter(Collider other)
+    {
+        playerInZone = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        playerInZone = false;
+    }
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && playerInZone)
         {
             toggleBoxes();
         }
